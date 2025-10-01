bc29503d2a3533ce3aecb42a6cb83236e14b5183
nylinUA <nylin@ualberta.ca>
2025-10-01 17:41:18 -0600

    GREEN fixed employee.java code

--- src/main/java/city/service/Employee.java
+++ src/main/java/city/service/Employee.java
@@ -15,4 +15,7 @@ public class Employee {
         this.salary = salary;
         this.address = address;
     }
+    public String getCapitalizedName() {
+        return name.substring(0, 1).toUpperCase()+name.substring(1).toLowerCase();
+    }
 }
